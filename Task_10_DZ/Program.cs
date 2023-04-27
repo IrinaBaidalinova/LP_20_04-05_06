5cec22cd846e30da372f281dd91b39aa2b229779
Irina <irishka_90_26@mail.ru>
2023-04-27 23:55:09 +0600

    Добавили папку Task_10_DZ

---
+++ Task_10_DZ/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу,
+// Которая принимает на вход трехзначное число
+// и на выходе показывает вторую 
+
+
+Console.WriteLine("Введите трехзначное число");
+while (true)
+{
+
+   string input = Console.ReadLine();
+   if (!input.Equals("exit"))
+      Console.WriteLine("{0}->{1}", input, input[1]);
+   else
+      break;
+}
