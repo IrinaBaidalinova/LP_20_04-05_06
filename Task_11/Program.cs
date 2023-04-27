31edd0739c84a42b2f76d813077808eab5dbc573
Irina <irishka_90_26@mail.ru>
2023-04-27 22:37:44 +0600

    Добавили папки Task_11 и Task_12

---
+++ Task_11/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 14 Напишите программу,
+// 1. Которая принимает на вход число
+//2. и проверяет,кратно ли оно одновременно 7 и 23
+// Пример:
+// 14->нет
+// 46->нет
+//161-> да
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = Multiplicity(number);
+Console.WriteLine(result ? "да" : "нет");
+bool Multiplicity(int num)
+{
+   return num % 7 == 0 && num % 23 == 0;
+}
