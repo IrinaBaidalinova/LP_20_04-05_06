7927fc4efd0cc178e6f561fd7307a5ab7ca8a000
Irina <irishka_90_26@mail.ru>
2023-05-03 23:31:45 +0600

    Добавили папки Task_17,18,20,22

---
+++ Task_18/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 18: 
+//1 Напишите программу, которая по
+// 2  заданному номеру четверти, показывает диапазон
+// 3  возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти:");
+int input = Convert.ToInt32(Console.ReadLine());
+string range = Range(input);
+Console.WriteLine(range == null ? "Некорректный ввод " : range);
+
+string Range (int numb)
+{
+   if (numb == 1) return "x > 0, y > 0";
+   if (numb == 2) return "x < 0, y > 0";
+   if (numb == 3) return "x < 0, y < 0";
+   if (numb == 4) return "x > 0, y < 0";
+   return null;
+}
---
+++ Task_22/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 22:
+//1 Напишите программу,
+//2 которая принимает на вход
+//3 число (N) и выдаёт таблицу
+//4 квадратов чисел от 1 до N.
+// Например:
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+Console.Write("Введите положительное целое число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+
+Square(numberN);
+
+void Square(int num)
+{
+int count = 1;
+while(count <= num)
+{
+   Console.WriteLine($"{count,3} {count * count,5}");
+   count ++;
+}
+
+}
+
