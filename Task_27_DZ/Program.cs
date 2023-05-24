f471e38596526e69c969cd63304b1d88e4ce5e49
Irina <irishka_90_26@mail.ru>
2023-05-24 23:13:34 +0600

    Добавили папки Task 25,27,29DZ

---
+++ Task_27_DZ/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27:
+//1 Напишите программу, 
+//2 которая принимает на вход число 
+//3 и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+Console.WriteLine("Введите целое положительное число: ");
+
+int x = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 2; i >= 0; i--)
+{
+   sum += x / (int)Math.Pow(10.0, i);
+   x = x % (int)Math.Pow(10.0, i);
+}
+Console.WriteLine(sum);
+
---
+++ Task_29DZ/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29:
+//1. Напишите программу, которая задаёт
+//2.массив из 8 элементов, заполненный псевдослучайными числами
+//3. и выводит их на экран.
+//Пример:
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+
+//int[] a = Enumerable.Range(1, 8).Select(x => 17 % x).ToArray();
+//foreach (int i in a)
+//Console.WriteLine(i);
+
+
+int[] randomArray = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+randomArray[i] = new Random().Next(1,8);
+Console.Write(randomArray[i] + " ");
+}
+
