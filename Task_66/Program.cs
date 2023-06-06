a0f5bac94910a981c2740da1c8820aa7d02c4b6d
Irina <irishka_90_26@mail.ru>
2023-06-06 21:57:47 +0600

    Добавили папки 64,66,68

---
+++ Task_66/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 66:
+//1. Задайте значения M и N.
+//2. Напишите программу, которая найдёт сумму натуральных элементов
+//3. в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int n, m, s = 0;
+Console.Write("Введи значение M = ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение N = ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = n; i <= m; i++)
+{
+   if (i % 1 == 0)
+      s = s + i ;   //целое число
+
+
+}
+Console.WriteLine(s);
