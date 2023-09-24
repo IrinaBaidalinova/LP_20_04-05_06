7e992a75f25962e72512db8dbda008ef9c3ce40e
Irina <irishka_90_26@mail.ru>
2023-09-24 21:16:57 +0600

    Написали программу которая из имеющегося массива строк формирует новый массив строк, длина которых меньше, либо равна 3 строкам клавиатуры

--- КОНТРОЛЬНАЯ РАБОТА/Program.cs
+++ КОНТРОЛЬНАЯ РАБОТА/Program.cs
@@ -23,8 +23,7 @@
 
 string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2);
-
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
@@ -35,3 +34,14 @@ void SecondArrayWithIF(string[] array1, string[] array2);
         count++;
         }
     }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1,array2);
+PrintArray(array2);
